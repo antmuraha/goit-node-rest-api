308c2c85647356fb54e47934d601dbbb03c4f5ac
Ruslan Kryvko <ruslan.muraha19@gmail.com>
2026-02-09 21:19:25 +0200

    Add validation schemas for contact creation and updates

--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -1,9 +1,31 @@
 import Joi from "joi";
 
-export const createContactSchema = Joi.object({
+const name = Joi.string().max(100);
+const email = Joi.string().email();
+const phone = Joi.string()
+    .pattern(/^\(\d{3}\) \d{3}-\d{4}$/)
+    .message("Phone number must be in the format (XXX) XXX-XXXX");
 
-})
+export const createContactSchema = Joi.object({
+    name: name.required(),
+    email: email.required(),
+    phone: phone.required(),
+});
+
+export const partialUpdateContactSchema = Joi.object({
+    name,
+    email,
+    phone,
+}).or("name", "email", "phone");
 
 export const updateContactSchema = Joi.object({
+    name: name.required(),
+    email: email.required(),
+    phone: phone.required(),
+});
 
-})
+export default {
+    createContactSchema,
+    updateContactSchema,
+    partialUpdateContactSchema,
+};
