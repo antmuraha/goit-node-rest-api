5a4bf6e285d2fcf8db54a28521512566d5bf58cb
Ruslan Kryvko <ruslan.muraha19@gmail.com>
2026-02-09 21:18:06 +0200

    Refactor HTTP status codes to use constants for better maintainability

--- helpers/HttpError.js
+++ helpers/HttpError.js
@@ -1,15 +1,25 @@
+export const HTTP_STATUS_CODES = {
+    SUCCESS: 200,
+    CREATED: 201,
+    BAD_REQUEST: 400,
+    UNAUTHORIZED: 401,
+    FORBIDDEN: 403,
+    NOT_FOUND: 404,
+    CONFLICT: 409,
+};
+
 const messageList = {
-    400: "Bad Request",
-    401: "Unauthorized",
-    403: "Forbidden",
-    404: "Not Found",
-    409: "Conflict",
-}
+    [HTTP_STATUS_CODES.BAD_REQUEST]: "Bad Request",
+    [HTTP_STATUS_CODES.UNAUTHORIZED]: "Unauthorized",
+    [HTTP_STATUS_CODES.FORBIDDEN]: "Forbidden",
+    [HTTP_STATUS_CODES.NOT_FOUND]: "Not Found",
+    [HTTP_STATUS_CODES.CONFLICT]: "Conflict",
+};
 
 const HttpError = (status, message = messageList[status]) => {
     const error = new Error(message);
     error.status = status;
     return error;
-}
+};
 
 export default HttpError;
