f793a1d7b91a16528b5995bc202f1b8143463d8a
Ruslan Kryvko <ruslan.muraha19@gmail.com>
2026-02-11 14:41:17 +0200

    update express to version 5.2.1 and adjust dependencies accordingly
    refactor: streamline contact controller methods and remove try-catch blocks

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,69 +1,45 @@
 import contactsService from "../services/contactsServices.js";
 import HttpError, { HTTP_STATUS_CODES } from "../helpers/HttpError.js";
 
-export const getAllContacts = async (req, res, next) => {
-    try {
-        const contacts = await contactsService.listContacts();
-        res.status(HTTP_STATUS_CODES.SUCCESS).json(contacts);
-    } catch (error) {
-        next(error);
-    }
+export const getAllContacts = async (req, res) => {
+    const contacts = await contactsService.listContacts();
+    res.status(HTTP_STATUS_CODES.SUCCESS).json(contacts);
 };
 
 export const getOneContact = async (req, res, next) => {
-    try {
-        const contact = await contactsService.getContactById(req.params.id);
-        if (!contact) {
-            return next(HttpError(HTTP_STATUS_CODES.NOT_FOUND));
-        }
-        res.status(HTTP_STATUS_CODES.SUCCESS).json(contact);
-    } catch (error) {
-        next(error);
+    const contact = await contactsService.getContactById(req.params.id);
+    if (!contact) {
+        return next(HttpError(HTTP_STATUS_CODES.NOT_FOUND));
     }
+    res.status(HTTP_STATUS_CODES.SUCCESS).json(contact);
 };
 
 export const deleteContact = async (req, res, next) => {
-    try {
-        const contact = await contactsService.removeContact(req.params.id);
-        if (!contact) {
-            return next(HttpError(HTTP_STATUS_CODES.NOT_FOUND));
-        }
-        res.status(HTTP_STATUS_CODES.SUCCESS).json(contact);
-    } catch (error) {
-        next(error);
+    const contact = await contactsService.removeContact(req.params.id);
+    if (!contact) {
+        return next(HttpError(HTTP_STATUS_CODES.NOT_FOUND));
     }
+    res.status(HTTP_STATUS_CODES.SUCCESS).json(contact);
 };
 
-export const createContact = async (req, res, next) => {
-    try {
-        const { name, email, phone } = req.body;
-        const newContact = await contactsService.addContact(name, email, phone);
-        res.status(HTTP_STATUS_CODES.CREATED).json(newContact);
-    } catch (error) {
-        next(error);
-    }
+export const createContact = async (req, res) => {
+    const { name, email, phone } = req.body;
+    const newContact = await contactsService.addContact(name, email, phone);
+    res.status(HTTP_STATUS_CODES.CREATED).json(newContact);
 };
 
 export const updateContact = async (req, res, next) => {
-    try {
-        const updatedContact = await contactsService.updateContact(req.params.id, req.body);
-        if (!updatedContact) {
-            return next(HttpError(HTTP_STATUS_CODES.NOT_FOUND));
-        }
-        res.status(HTTP_STATUS_CODES.SUCCESS).json(updatedContact);
-    } catch (error) {
-        next(error);
+    const updatedContact = await contactsService.updateContact(req.params.id, req.body);
+    if (!updatedContact) {
+        return next(HttpError(HTTP_STATUS_CODES.NOT_FOUND));
     }
+    res.status(HTTP_STATUS_CODES.SUCCESS).json(updatedContact);
 };
 
 export const updateStatusContact = async (req, res, next) => {
-    try {
-        const updatedContact = await contactsService.updateContact(req.params.id, req.body);
-        if (!updatedContact) {
-            return next(HttpError(HTTP_STATUS_CODES.NOT_FOUND));
-        }
-        res.status(HTTP_STATUS_CODES.SUCCESS).json(updatedContact);
-    } catch (error) {
-        next(error);
+    const updatedContact = await contactsService.updateContact(req.params.id, req.body);
+    if (!updatedContact) {
+        return next(HttpError(HTTP_STATUS_CODES.NOT_FOUND));
     }
+    res.status(HTTP_STATUS_CODES.SUCCESS).json(updatedContact);
 };
